40ee71583a1405f9d2d4b87387bd269ebc44fb52
Tschokko <tschokko@gmx.de>
2019-07-11 18:35:27 +0000

    Added severall messages.

--- include/protocol.hpp
+++ include/protocol.hpp
@@ -15,59 +15,344 @@ namespace iotcore::devicecontrol::v1::protocol {
 enum MessageTypes {
   kMessageTypeInvalid = 0,
   kMessageTypeHello = 1,
-  kMessageTypeWelcome = 2
+  kMessageTypeWelcome = 2,
+  kMessageTypeAbort = 3,
+  kMessageTypePing = 4,
+  kMessageTypePong = 5,
 };
-
-namespace placeholders {
-
-class Placeholder {};
-void to_json(json& j, const Placeholder&) { j = json::object(); }
-
-}  // namespace placeholders
 
 namespace hellomessage {
 
-using iotcore::devicecontrol::v1::protocol::placeholders::Placeholder;
-
-template <typename T = json>
+// The representation of the websocket hello message (message type 1).
 class HelloMessage {
+  std::string realm_;
+  json details_;
+
+  auto tie() const { return std::tie(realm_, details_); }
+
+  friend void to_json(json& j, const HelloMessage& msg);
+  friend void from_json(const json& j, HelloMessage& msg);
+
  public:
   HelloMessage() {}
-  HelloMessage(const std::string& realm) : realm_(realm) {}
+  explicit HelloMessage(const std::string& realm)
+      : realm_(realm), details_(json::object()) {}
+  HelloMessage(const std::string& realm, const json& details)
+      : realm_(realm), details_(details) {}
+  template <typename T>
   HelloMessage(const std::string& realm, const T& details)
       : realm_(realm), details_(details) {}
 
+  inline bool operator==(const HelloMessage& rhs) const {
+    return tie() == rhs.tie();
+  }
+
   // accessors
-  const std::string& realm() { return realm_; }
-  const T& details() { return details_; }
+  auto realm() -> const std::string& { return realm_; }
+  auto details() -> const json& { return details_; }
+  template <typename T>
+  auto details() const {
+    return details_.get<T>();
+  }
 
- private:
-  template <typename T_>
-  friend void to_json(json& j, const HelloMessage<T_>& msg);
-  template <typename T_>
-  friend void from_json(const json& j, HelloMessage<T_>& msg);
-
-  std::string realm_;
-  T details_;
+  // mutators
+  void set_realm(const std::string& realm) { realm_ = realm; }
+  void set_details(const json& details) { details_ = details; }
+  template <typename T>
+  void set_details(const T& details) {
+    details_ = details;
+  }
 };
 
-template <typename T>
-void to_json(json& j, const HelloMessage<T>& msg) {
+void to_json(json& j, const HelloMessage& msg) {
   j.push_back(kMessageTypeHello);
   j.push_back(msg.realm_);
   j.push_back(msg.details_);
 }
 
-template <typename T>
-void from_json(const json& j, HelloMessage<T>& msg) {
+void from_json(const json& j, HelloMessage& msg) {
   // j.at("name").get_to(p.name);
-  const auto realm = j[1];
-  const auto details = j[2];
-  msg.realm_ = realm;
-  msg.details_ = details;
+  msg.realm_ = j[1];
+  msg.details_ = j[2];
+
+  if (msg.details_.is_null()) {
+    msg.details_ = json::object();
+  }
 }
 
 }  // namespace hellomessage
+
+// Returns an instance of hellomessage::HelloMessage with given realm.
+hellomessage::HelloMessage HelloMessage(const std::string realm) {
+  return hellomessage::HelloMessage(realm);
+}
+
+// Returns an instance of hellomessage::HelloMessage with given realm and
+// details.
+template <typename T>
+hellomessage::HelloMessage HelloMessage(const std::string realm,
+                                        const T& details) {
+  return hellomessage::HelloMessage(realm, details);
+}
+
+namespace welcomemessage {
+
+// The representation of the websocket welcome message (message type 2).
+class WelcomeMessage {
+  int32_t session_id_;
+  json details_;
+
+  auto tie() const { return std::tie(session_id_, details_); }
+
+  friend void to_json(json& j, const WelcomeMessage& msg);
+  friend void from_json(const json& j, WelcomeMessage& msg);
+
+ public:
+  WelcomeMessage() {}
+  explicit WelcomeMessage(int32_t session_id)
+      : session_id_(session_id), details_(json::object()) {}
+  WelcomeMessage(int32_t session_id, const json& details)
+      : session_id_(session_id), details_(details) {}
+  template <typename T>
+  WelcomeMessage(int32_t session_id, const T& details)
+      : session_id_(session_id), details_(details) {}
+
+  inline bool operator==(const WelcomeMessage& rhs) const {
+    return tie() == rhs.tie();
+  }
+
+  // accessors
+  auto session_id() -> int32_t { return session_id_; }
+  auto details() -> const json& { return details_; }
+  template <typename T>
+  auto details() const {
+    return details_.get<T>();
+  }
+
+  // mutators
+  void set_session_id(int32_t session_id) { session_id_ = session_id; }
+  void set_details(const json& details) { details_ = details; }
+  template <typename T>
+  void set_details(const T& details) {
+    details_ = details;
+  }
+};
+
+void to_json(json& j, const WelcomeMessage& msg) {
+  j.push_back(kMessageTypeWelcome);
+  j.push_back(msg.session_id_);
+  j.push_back(msg.details_);
+}
+
+void from_json(const json& j, WelcomeMessage& msg) {
+  // j.at("name").get_to(p.name);
+  msg.session_id_ = j[1];
+  msg.details_ = j[2];
+
+  if (msg.details_.is_null()) {
+    msg.details_ = json::object();
+  }
+}
+
+}  // namespace welcomemessage
+
+// Returns an instance of welcomemessage::WelcomeMessage with given session_id.
+welcomemessage::WelcomeMessage WelcomeMessage(int32_t session_id) {
+  return welcomemessage::WelcomeMessage(session_id);
+}
+
+// Returns an instance of welcomemessage::WelcomeMessage with given session_id
+// and details.
+template <typename T>
+welcomemessage::WelcomeMessage WelcomeMessage(int32_t session_id,
+                                              const T& details) {
+  return welcomemessage::WelcomeMessage(session_id, details);
+}
+
+namespace abortmessage {
+
+// The representation of the websocket abort message (message type 3).
+class AbortMessage {
+  std::string reason_;
+  json details_;
+
+  auto tie() const { return std::tie(reason_, details_); }
+
+  friend void to_json(json& j, const AbortMessage& msg);
+  friend void from_json(const json& j, AbortMessage& msg);
+
+ public:
+  AbortMessage() {}
+  explicit AbortMessage(const std::string& reason)
+      : reason_(reason), details_(json::object()) {}
+  AbortMessage(const std::string& reason, const json& details)
+      : reason_(reason), details_(details) {}
+  template <typename T>
+  AbortMessage(const std::string& reason, const T& details)
+      : reason_(reason), details_(details) {}
+
+  inline bool operator==(const AbortMessage& rhs) const {
+    return tie() == rhs.tie();
+  }
+
+  // accessors
+  auto reason() -> const std::string& { return reason_; }
+  auto details() -> const json& { return details_; }
+  template <typename T>
+  auto details() const {
+    return details_.get<T>();
+  }
+
+  // mutators
+  void set_reason(const std::string& reason) { reason_ = reason; }
+  void set_details(const json& details) { details_ = details; }
+  template <typename T>
+  void set_details(const T& details) {
+    details_ = details;
+  }
+};
+
+void to_json(json& j, const AbortMessage& msg) {
+  j.push_back(kMessageTypeAbort);
+  j.push_back(msg.reason_);
+  j.push_back(msg.details_);
+}
+
+void from_json(const json& j, AbortMessage& msg) {
+  // j.at("name").get_to(p.name);
+  msg.reason_ = j[1];
+  msg.details_ = j[2];
+
+  if (msg.details_.is_null()) {
+    msg.details_ = json::object();
+  }
+}
+
+}  // namespace abortmessage
+
+namespace pingmessage {
+
+// The representation of the websocket ping message (message type 4).
+class PingMessage {
+  json details_;
+
+  auto tie() const { return std::tie(details_); }
+
+  friend void to_json(json& j, const PingMessage& msg);
+  friend void from_json(const json& j, PingMessage& msg);
+
+ public:
+  PingMessage() {}
+  PingMessage(const json& details) : details_(details) {}
+  template <typename T>
+  PingMessage(const T& details) : details_(details) {}
+
+  inline bool operator==(const PingMessage& rhs) const {
+    return tie() == rhs.tie();
+  }
+
+  // accessors
+  auto details() -> const json& { return details_; }
+  template <typename T>
+  auto details() const {
+    return details_.get<T>();
+  }
+
+  // mutators
+  void set_details(const json& details) { details_ = details; }
+  template <typename T>
+  void set_details(const T& details) {
+    details_ = details;
+  }
+};
+
+void to_json(json& j, const PingMessage& msg) {
+  j.push_back(kMessageTypePing);
+  j.push_back(msg.details_);
+}
+
+void from_json(const json& j, PingMessage& msg) {
+  // j.at("name").get_to(p.name);
+  msg.details_ = j[1];
+
+  if (msg.details_.is_null()) {
+    msg.details_ = json::object();
+  }
+}
+
+}  // namespace pingmessage
+
+// Returns an instance of abortmessage::PingMessage with given reason.
+pingmessage::PingMessage PingMessage() { return pingmessage::PingMessage(); }
+
+// Returns an instance of abortmessage::PingMessage with given reason and
+// details.
+template <typename T>
+pingmessage::PingMessage PingMessage(const T& details) {
+  return pingmessage::PingMessage(details);
+}
+
+namespace pongmessage {
+
+// The representation of the websocket ping message (message type 4).
+class PongMessage {
+  json details_;
+
+  auto tie() const { return std::tie(details_); }
+
+  friend void to_json(json& j, const PongMessage& msg);
+  friend void from_json(const json& j, PongMessage& msg);
+
+ public:
+  PongMessage() {}
+  PongMessage(const json& details) : details_(details) {}
+  template <typename T>
+  PongMessage(const T& details) : details_(details) {}
+
+  inline bool operator==(const PongMessage& rhs) const {
+    return tie() == rhs.tie();
+  }
+
+  // accessors
+  auto details() -> const json& { return details_; }
+  template <typename T>
+  auto details() const {
+    return details_.get<T>();
+  }
+
+  // mutators
+  void set_details(const json& details) { details_ = details; }
+  template <typename T>
+  void set_details(const T& details) {
+    details_ = details;
+  }
+};
+
+void to_json(json& j, const PongMessage& msg) {
+  j.push_back(kMessageTypePing);
+  j.push_back(msg.details_);
+}
+
+void from_json(const json& j, PongMessage& msg) {
+  // j.at("name").get_to(p.name);
+  msg.details_ = j[1];
+
+  if (msg.details_.is_null()) {
+    msg.details_ = json::object();
+  }
+}
+
+}  // namespace pongmessage
+
+// Returns an instance of abortmessage::PingMessage with given reason.
+pongmessage::PongMessage PongMessage() { return pongmessage::PongMessage(); }
+
+// Returns an instance of abortmessage::PingMessage with given reason and
+// details.
+template <typename T>
+pongmessage::PongMessage PongMessage(const T& details) {
+  return pongmessage::PongMessage(details);
+}
 
 }  // namespace iotcore::devicecontrol::v1::protocol
 
--- src/hello.cpp
+++ src/hello.cpp
@@ -7,17 +7,28 @@
 
 namespace details {
 class Details {
- public:
-  Details(const std::string& serial) : serial_(serial) {}
-
- private:
-  friend void to_json(json& j, const Details&);
-
   std::string serial_;
+
+  auto tie() const { return std::tie(serial_); }
+
+  friend void to_json(json& j, const Details&);
+  friend void from_json(const json& j, Details&);
+
+ public:
+  Details() {}
+  Details(const std::string& serial) : serial_(serial){};
+
+  inline bool operator==(const Details& rhs) const {
+    return tie() == rhs.tie();
+  }
 };
 
 void to_json(json& j, const Details& details) {
-  j["serial_number"] = details.serial_;
+  j = json{{"serial_number", details.serial_}};
+}
+
+void from_json(const json& j, Details& details) {
+  j.at("serial_number").get_to(details.serial_);
 }
 
 }  // namespace details
@@ -25,7 +36,6 @@ void to_json(json& j, const Details& details) {
 int main() {
   using json = nlohmann::json;
   using namespace iotcore::devicecontrol::v1::protocol;
-  using iotcore::devicecontrol::v1::protocol::placeholders::Placeholder;
 
   /*auto j = json::parse(
       "[2, 293847, {\"session_timeout\": 3600, \"ping_interval\": 3584, "
@@ -33,21 +43,30 @@ int main() {
   std::cout << j << std::endl; */
 
 
-  auto msg = hellomessage::HelloMessage{"test@test"};
+  // auto msg = hellomessage::HelloMessage{"test@test"};
+  auto msg = HelloMessage("test@test");
   json j = msg;
 
-  auto msg2 = hellomessage::HelloMessage<details::Details>{
-      "test2@test2", details::Details{"1234"}};
+  // auto msg2 = hellomessage::HelloMessage{"test2@test2"};
+  auto msg2 = HelloMessage("test2@test2", details::Details{"5678"});
+  // msg2.set_details(details::Details{"5678"});
   json j2 = msg2;
 
-  auto j3 = json::parse("[2, \"test3@test3\", {}]");
-  auto msg3 = j3.get<hellomessage::HelloMessage<json>>();
+  auto j3 = json::parse("[2, \"test3@test3\"]");
+  auto msg3 = j3.get<hellomessage::HelloMessage>();
+
+  auto j4 = json::parse("[2, \"test3@test3\", {\"serial_number\": \"1234\"}]");
+  auto msg4 = j4.get<hellomessage::HelloMessage>();
 
   std::cout << j << std::endl;
   std::cout << j2 << std::endl;
   std::cout << j3 << std::endl;
+  std::cout << j4 << std::endl;
 
   std::cout << msg3.realm() << ", " << msg3.details() << std::endl;
+
+  std::cout << msg4.realm() << ", " << msg4.details() << std::endl;
+  auto details = msg4.details<details::Details>();
 
   return 0;
 }
