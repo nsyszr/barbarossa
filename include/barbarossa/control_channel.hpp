4cb2e1cd381364b40bf532030a399bc7c0e20bae
Djordje Gladovic <dgladovic@insys-icom.de>
2019-07-19 15:56:26 +0200

    Ping / Pong is back again. :)

--- include/barbarossa/control_channel.hpp
+++ include/barbarossa/control_channel.hpp
@@ -1,12 +1,14 @@
 #ifndef CONTROLCHANNEL_HPP_
 #define CONTROLCHANNEL_HPP_
 
+#include <condition_variable>
 #include <cstdint>
 #include <functional>
 #include <future>
 #include <mutex>
 
 #include "barbarossa/protocol.hpp"
+#include "barbarossa/protocol_details.hpp"
 #include "barbarossa/zmq_utils.hpp"
 #include "nlohmann/json.hpp"
 #include "spdlog/spdlog.h"
@@ -166,20 +168,20 @@ class ControlChannel {
           return;
         }
         case kControlChannelEventOnOpen: {
-          TransitionStateTo(kControlChannelStateOpened);
+          NextState(kControlChannelStateOpened);
           break;
         }
         case kControlChannelEventOnClose: {
-          TransitionStateTo(kControlChannelStateClosed);
+          NextState(kControlChannelStateClosed);
           break;
         }
         case kControlChannelEventOnFail: {
-          TransitionStateTo(kControlChannelStateClosed);
+          NextState(kControlChannelStateClosed);
           break;
         }
         case kControlChannelEventOnMessage: {
-          /*auto msg = request.popstr();
-          HandleMessage(msg);*/
+          auto payload = request.popstr();
+          NextState(HandleEventOnMessage(payload));
           break;
         }
         case kControlChannelEventOnTimeout: {
@@ -196,19 +198,46 @@ class ControlChannel {
     }
   }
 
+  // acccesors
+  // mutators
+  void set_session_id(int32_t session_id) { session_id_ = session_id; }
+  void set_session_timeout(int session_timeout) {
+    session_timeout_ = session_timeout;
+  }
+  void set_ping_interval(int ping_interval) { ping_interval_ = ping_interval; }
+  void set_pong_timeout(int pong_timeout) { pong_timeout_ = pong_timeout; }
+  void set_events_topic(const std::string events_topic) {
+    events_topic_ = events_topic;
+  }
+
  private:
+  // Keep the order of ctor initialization
   T& endpoint_;
   ControlChannelStates state_;
   zmq::context_t context_;
 
+  // Other private member variables
+  std::mutex state_mutex_;
   std::mutex raise_event_mutex_;
 
+  std::condition_variable hello_message_reply_;
+  std::mutex hello_message_reply_mutex_;
+
+  std::condition_variable heartbeat_;
+  std::mutex heartbeat_mutex_;
+
+
   int32_t session_id_;
+  int session_timeout_;
+  int ping_interval_;
+  int pong_timeout_;
+  std::string events_topic_;
 
   void EndpointOnOpen() { RaiseEvent(kControlChannelEventOnOpen); }
   void EndpointOnClose() { RaiseEvent(kControlChannelEventOnClose); }
   void EndpointOnFail() { RaiseEvent(kControlChannelEventOnFail); }
   void EndpointOnMessage(const std::string& payload) {
+    spdlog::debug("control_channel: Message payload: '{}'", payload);
     RaiseEvent(kControlChannelEventOnMessage, payload);
   }
 
@@ -241,24 +270,144 @@ class ControlChannel {
     request.send(socket);
   }
 
-  // Above methods are okay
-  void HandleStates() {
-    spdlog::debug("control_channel: Handle state: {}", AsString(state_));
+  ControlChannelStates HandleEventOnMessage(const std::string& payload) {
+    auto msg = protocol::Parse(payload);
+    if (msg.GetMessageType() == protocol::kMessageTypeWelcome) {
+      spdlog::debug("control_channel: Release hello message reply condition");
+      std::unique_lock<std::mutex> lock(hello_message_reply_mutex_);
+      hello_message_reply_.notify_one();
 
-    switch (state_) {
+      // Save the settings of the welcome message
+      auto welcome_msg = msg.Get<protocol::welcomemessage::WelcomeMessage>();
+      set_session_id(welcome_msg.session_id());
+
+      auto welcome_msg_details =
+          welcome_msg.details<protocol::welcomedetails::WelcomeDetails>();
+      set_session_timeout(welcome_msg_details.session_timeout());
+      set_ping_interval(welcome_msg_details.ping_interval());
+      set_pong_timeout(welcome_msg_details.pong_timeout());
+      set_events_topic(welcome_msg_details.events_topic());
+
+      SetupHeartbeat();
+
+      return kControlChannelStateEstablished;
+    } else if (msg.GetMessageType() == protocol::kMessageTypeAbort) {
+      ControlChannelStates state;
+      // Release mutex inside scope
+      {
+        std::lock_guard<std::mutex> lock(state_mutex_);
+        state = state_;
+      }
+
+      if (state == kControlChannelStateOpened) {
+        return kControlChannelStateRefused;
+      } else {
+        return kControlChannelStateAborted;
+      }
+    } else if (msg.GetMessageType() == protocol::kMessageTypePong) {
+      auto socket = zmqutils::Connect(context_, "inproc://pong", ZMQ_PAIR);
+      zmqutils::SendString(socket, "PONG");
+      return kControlChannelStateEstablished;
+    }
+  }
+
+  void SetupHeartbeat() {
+    std::thread([&]() {
+      std::chrono::seconds ping_interval(ping_interval_);
+      std::chrono::seconds pong_timeout(pong_timeout_);
+
+      while (true) {
+        std::unique_lock<std::mutex> lock(heartbeat_mutex_);
+
+        spdlog::debug("Wait until sending ping or die.");
+        // We send every given seconds a ping or we stop the hearbeat
+        if (heartbeat_.wait_for(lock, ping_interval) ==
+            std::cv_status::no_timeout) {
+          spdlog::info(
+              "contorl_channel: Terminate heartbeat. Received the stop "
+              "condition.");
+          break;  // Exit the loop because stop_heartbeat condition is set
+        }
+
+        // Send ping
+        spdlog::debug("contorl_channel: Sending ping.");
+        /*auto ping_socket =
+            zmqutils::Connect(context_, "inproc://ping", ZMQ_PAIR);
+        zmqutils::SendString(ping_socket, "PING"); */
+        auto ping_msg = protocol::PingMessage();
+        json j = ping_msg;
+        endpoint_.Send(j.dump());
+
+        // Create a async wait ping reply routine which returns a future
+        auto request = std::async(
+            [](zmq::context_t& context, std::chrono::seconds pong_timeout) {
+              // auto context = zmq::context_t(1);
+              auto socket = zmqutils::Bind(context, "inproc://pong", ZMQ_PAIR);
+
+              zmq::pollitem_t items[] = {{socket, 0, ZMQ_POLLIN, 0}};
+
+              zmq::poll(&items[0], 1, pong_timeout);
+
+              if (items[0].revents & ZMQ_POLLIN) {
+                auto msg = zmqutils::RecvString(socket);
+                spdlog::info("Got message: {}", msg);
+                return true;
+              }
+
+              return false;
+            },
+            std::ref(context_), std::chrono::seconds(pong_timeout_));
+
+        // Wait until we get a reply
+        spdlog::debug("contorl_channel: Waiting for pong.");
+        bool reply = request.get();
+        spdlog::debug("contorl_channel: Pong result: {}");
+
+        if (reply == false) {
+          spdlog::warn(
+              "contorl_channel: Terminate heartbeat. We didn't received a "
+              "pong.");
+          break;  // Exit the loop because ping wasn't replied within time.
+        }
+      }
+    })
+        .detach();
+  }
+
+  // Above methods are okay
+  void NextState(ControlChannelStates state) {
+    spdlog::debug("control_channel: Handle state from {} to {}.",
+                  AsString(state_), AsString(state));
+
+    ControlChannelStates current_state;
+
+    // To release the mutex we change the state inside a scope
+    {
+      std::lock_guard<std::mutex> lock(state_mutex_);
+
+      if (state == state_) {
+        // State didn't changed, nothing todo
+        spdlog::debug("control_channel: Warning! State didn't changed");
+        return;
+      }
+
+      state_ = state;
+      current_state = state;
+    }
+
+    switch (current_state) {
       case kControlChannelStatePending: {
         // Initial state, nothing to do!
         break;
       }
       case kControlChannelStateOpened: {
         // Send hello message to endpoint
-        auto hello_msg = protocol::HelloMessage("barbarossa@test");
-        json j = hello_msg;
-        endpoint_.Send(j.dump());
+        HandleStateOpened();
         break;
       }
       case kControlChannelStateEstablished: {
         // Start keep alive
+        HandleStateEstablished();
         break;
       }
       case kControlChannelStateRefused: {
@@ -288,14 +437,33 @@ class ControlChannel {
     }
   }
 
-  void TransitionStateTo(ControlChannelStates state) {
-    spdlog::warn("Change state from {} to {}.", AsString(state_),
-                 AsString(state));
-    state_ = state;
-    HandleStates();
+  void HandleStateOpened() {
+    // Send hello message to server
+    auto hello_msg = protocol::HelloMessage("barbarossa@test");
+    json j = hello_msg;
+    endpoint_.Send(j.dump());
+
+    // Start a thread that waits for the reply or timeout
+    std::thread(
+        [&](std::chrono::seconds timeout) {
+          spdlog::debug("control_channel: Wait for hello message reply.");
+
+          std::unique_lock<std::mutex> lock(hello_message_reply_mutex_);
+          if (hello_message_reply_.wait_for(lock, timeout) ==
+              std::cv_status::timeout) {
+            spdlog::debug("control_channel: Timeout hello message reply.");
+            RaiseEvent(kControlChannelEventOnTimeout,
+                       "HELLO");  // TODO(DGL) Replace string
+          }
+        },
+        std::chrono::seconds(4))
+        .detach();  // TODO(DGL) Replace with a settings variable
   }
 
-  // void TransitionStateTo(ControlChannelStates state);
+  void HandleStateEstablished() {
+    // Start the hearbeat
+  }
+
   // void HandleMessage(const std::string& payload);
 
   // int EstablishSession();
---
+++ include/barbarossa/protocol_details.hpp
@@ -0,0 +1,78 @@
+#ifndef PROTOCOL_DETAILS_HPP__
+#define PROTOCOL_DETAILS_HPP__
+
+#include <string>
+#include <string_view>
+
+#include "nlohmann/json.hpp"
+
+using json = nlohmann::json;
+
+namespace barbarossa::controlchannel::v1::protocol {
+
+namespace welcomedetails {
+// The representation of the websocket hello message (message type 1).
+class WelcomeDetails {
+  int session_timeout_;
+  int ping_interval_;
+  int pong_timeout_;
+  std::string events_topic_;
+
+  auto tie() const {
+    return std::tie(session_timeout_, ping_interval_, pong_timeout_,
+                    events_topic_);
+  }
+
+  friend void to_json(json& j, const WelcomeDetails& d);
+  friend void from_json(const json& j, WelcomeDetails& d);
+
+ public:
+  WelcomeDetails()
+      : session_timeout_(120), ping_interval_(104), pong_timeout_(16) {}
+  WelcomeDetails(int session_timeout, int ping_interval, int pong_timeout,
+                 const std::string& events_topic)
+      : session_timeout_(session_timeout),
+        ping_interval_(ping_interval),
+        pong_timeout_(pong_timeout),
+        events_topic_(events_topic) {}
+
+  inline bool operator==(const WelcomeDetails& rhs) const {
+    return tie() == rhs.tie();
+  }
+
+  // accessors
+  auto session_timeout() -> int { return session_timeout_; }
+  auto ping_interval() -> int { return ping_interval_; }
+  auto pong_timeout() -> int { return pong_timeout_; }
+  auto events_topic() -> const std::string& { return events_topic_; }
+
+  // mutators
+  void set_session_timeout(int session_timeout) {
+    session_timeout_ = session_timeout;
+  }
+  void set_ping_interval(int ping_interval) { ping_interval_ = ping_interval; }
+  void set_pong_timeout(int pong_timeout) { pong_timeout_ = pong_timeout; }
+  void set_events_topic(const std::string& events_topic) {
+    events_topic_ = events_topic;
+  }
+};
+
+inline void to_json(json& j, const WelcomeDetails& d) {
+  j["session_timeout"] = d.session_timeout_;
+  j["ping_interval"] = d.ping_interval_;
+  j["pong_max_wait_time"] = d.pong_timeout_;
+  j["events_topic"] = d.events_topic_;
+}  // namespace welcomedetails
+
+inline void from_json(const json& j, WelcomeDetails& d) {
+  j.at("session_timeout").get_to(d.session_timeout_);
+  j.at("ping_interval").get_to(d.ping_interval_);
+  j.at("pong_max_wait_time").get_to(d.pong_timeout_);
+  j.at("events_topic").get_to(d.events_topic_);
+}
+
+}  // namespace welcomedetails
+
+}  // namespace barbarossa::controlchannel::v1::protocol
+
+#endif  // PROTOCOL_DETAILS_HPP__
